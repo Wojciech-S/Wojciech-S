1239d0c2d9a96bdec40772dfe4ff6017b0431e2e
Wojciech-S <62772289+Wojciech-S@users.noreply.github.com>
2020-03-29 16:20:00 +0200

    Add files via upload

---
+++ Listy.2.py
@@ -0,0 +1,10 @@
+dane = [8, -3, 22, 44, 29, -18, 0, -2]
+print(dane)
+
+for a in dane:
+    if a > 0:
+        print(f"Liczby dodatnie w zbiorze to: {a}")
+
+for b in dane:
+    if b < 0:
+        print(f"Liczby ujmene w zbiorze to: {b}")
