f3dd6cff837bdbb4648518b1501ec00f7dadf50c
Wojciech-S <62772289+Wojciech-S@users.noreply.github.com>
2020-04-17 17:46:32 +0200

    Add files via upload

--- Zadanie.md.py
+++ Zadanie.md.py
@@ -3,18 +3,18 @@ y = int(input("Podaj swoją współrzędną y: "))
 
 if (x < 0 or x > 100 or y < 0 or y > 100):
     print("Gracz znajduje się poza polem planszy.")
-elif (x > 10 and x < 90) and (y > 10 and y >= 90):
+elif (x > 10 and x < 90) and (y >= 90):
     print("Gracz znajduje się przy Górnej krawędzi.")
-elif (x > 10 and x < 90) and (y < 10):
+elif (x > 10 and x < 90) and (y <= 10):
     print("Gracz znajduje się przy Dolnej krawędzi.")
 elif (x <= 10 and y <= 10):
     print("Gracz znajduje się w Lewym dolnym rogu.")
-elif (x >= 90 and y <= 10):
-    print("Gracz znajduje się w Prawym dolnym rogu.")
 elif (x <= 10 and y >= 90):
     print("Gracz znajduje się w Lewym górny rogu.")
 elif (x <= 10 and y > 10 and y < 90):
     print("Gracz znajduje się przy Lewej krawędzi.")
+elif (x >= 90 and y <= 10):
+    print("Gracz znajduje się w Prawym dolnym rogu.")
 elif (x >= 90 and y >= 90):
     print("Gracz znajduje się w Prawym górnym rogu.")
 elif (x >= 90 and y > 10 and y < 90):
